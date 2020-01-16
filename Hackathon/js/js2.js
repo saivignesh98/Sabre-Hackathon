0c809f8ea5299fe7b4a78568af95af64b0d845c3
saivignesh98 <dragoonvignesh1998@gmail.com>
2020-01-17 00:30:05 +0530

    Update js2.js

--- Hackathon/js/js2.js
+++ Hackathon/js/js2.js
@@ -1,3 +1,7 @@
-function priceDisplay(){
-    
+function hidDisplay(){
+    x=document.getElementById("hid");
+    x.style.visibility="visible";
+    var e = document.getElementById("payments");
+    var t = e.options[e.selectedIndex].text;
+    x.innerHTML="You have selected "+t+" as your payment method.";
 }
